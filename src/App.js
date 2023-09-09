5a9ed03c1a4bf4ecc6c24a456921a1372492e817
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2023-09-09 11:17:42 +0530

    projects tabs created, need to fix response issu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Skills from './Components/Skills';
+import Projects from './Components/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -52,6 +52,25 @@ const Skills = () => {
             link: "ChatGPT"
         }
     ]
+
+    const links3 = [
+        {
+            id: 1,
+            link: "Figma"
+        },
+        {
+            id: 1,
+            link: "Photoshop"
+        },
+        {
+            id: 1,
+            link: "AdobeXd"
+        },
+        {
+            id: 1,
+            link: "illustrator"
+        }
+    ]
   return (
     <div name="Skills" className=' mobile:mt-16  desktop:mt-24 laptop:mt-14'>
         <div className='flex flex-col justify-center items-center'>
@@ -63,9 +82,9 @@ const Skills = () => {
 
         <div className='flex mobile:flex-col tablet:flex-row justify-center mobile:gap-8 tablet:gap-0 mt-16 laptop:mt-16 '>
             
-            <div className='outline outline-offset-2 outline-1 outline-black-500 rounded-lg mx-4 tablet:h-[22.5rem] tablet:w-[25rem] laptop:w-[25rem]'>
+            <div className='outline outline-offset-2 outline-1 outline-black-500 rounded-lg mx-4 tablet:h-[22.5rem] tablet:w-[25rem] laptop:w-[25rem] desktop:w-[34rem]'>
                     <h1 className='flex text-xl text-[#1D1D1D] font-semibold justify-center top-0 my-6'>Frontend development</h1>
-                <ul className='flex flex-wrap justify-center py-8 items-center gap-8'>
+                <ul className='flex flex-wrap justify-center py-10 items-center gap-8'>
                     {links.map(({link})=> (
                         <li className='flex align-baseline  my-4 '>
                             <img className='h-5 mx-2' src={verifiedlogo} alt='verifylogo'></img>
@@ -74,9 +93,9 @@ const Skills = () => {
                     ))}
                 </ul>
             </div>
-            <div className='justify-center items-center outline outline-offset-2 outline-1 outline-black-500 rounded-lg mx-4 tablet:h-[22.5rem] tablet:w-[25rem] laptop:w-[25rem]'>
+            <div className='justify-center items-center outline outline-offset-2 outline-1 outline-black-500 rounded-lg mx-4 tablet:h-[22.5rem] tablet:w-[25rem] laptop:w-[25rem] desktop:w-[34rem]'>
                 <h1 className='flex text-xl text-[#1D1D1D] justify-center font-semibold my-6'>Supporting tools</h1>
-                <ul className='flex flex-wrap justify-center items-center py-8 gap-8  '>
+                <ul className='flex flex-wrap justify-center items-center py-10 gap-8  '>
                     {links2.map(({link})=> (
                         <li className='flex align-baseline my-4'>
                             <img className='h-5 mx-2' src={verifiedlogo} alt='verifylogo'></img>
@@ -85,15 +104,20 @@ const Skills = () => {
                     ))}
                 </ul>
             </div>
-
-           
-
         </div>
-
-        <div className='h-[1000px]'></div>
-
-
-
+        <div className='flex mobile:flex-col tablet:flex-row justify-center mobile:gap-8 tablet:gap-0 mt-8 laptop:mt-6 '>
+            <div className=' justify-center items-center outline outline-offset-2 outline-1 outline-black-500 rounded-lg mx-4  tablet:w-[52rem] laptop:w-[52rem] desktop:w-[70rem]'>
+                    <h1 className='flex text-xl text-[#1D1D1D] justify-center font-semibold my-6'>Designing tools</h1>
+                    <ul className='flex flex-wrap justify-center items-center py-6 gap-8  '>
+                        {links3.map(({link})=> (
+                            <li className='flex align-baseline my-4'>
+                                <img className='h-5 mx-2' src={verifiedlogo} alt='verifylogo'></img>
+                                <p className='text-2 font-semibold text-[#454545]'>{link}</p>
+                            </li>
+                        ))}
+                    </ul>
+            </div>
+        </div>
     </div>
   )
 }
